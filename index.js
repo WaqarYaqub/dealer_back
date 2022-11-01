e74aea9f19cfe0c7d74294f38c8b6ef29e207889
snexusobi <obaid@snexus.com>
2022-11-01 17:19:13 +0500

    set db uri

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ console.log(process.env.DB_USER, "process.env.DB_USER");
 //MongoDB
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7qft9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.djx64nr.mongodb.net/?retryWrites=true&w=majority`;
-// const client = new MongoClient(process.env.MONGODB_URI, {
-const client = new MongoClient(uri, {
+const client = new MongoClient(process.env.MONGODB_URI, {
+// const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
